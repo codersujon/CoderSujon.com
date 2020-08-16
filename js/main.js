15229e970d25f8c26ce9987ea04fd6099830654f
sujon <csesujon155@gmail.com>
2020-08-16 15:40:42 -0700

    Starter

---
+++ js/main.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){
+	$('.single-slide-active').owlCarousel({
+	    loop:true,
+	    nav:false,
+	    dots: false,
+	    autoplay:true,
+	    responsive:{
+	        0:{
+	            items:1
+	        },
+	        600:{
+	            items:1
+	        },
+	        1000:{
+	            items:1
+	        }
+	    }
+	})
+
+	//Smooth Scroll
+	$('a').smoothScroll();
+	
+	//AOS Animation
+	AOS.init();
+
+});
+
